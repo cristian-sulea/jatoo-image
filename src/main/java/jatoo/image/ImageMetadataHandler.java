9b5a2e69ee2c89a5009fcd479c3a694514df3dc1
Cristian Sulea <cristian.sulea.79@gmail.com>
2017-05-26 16:32:39 +0300

    ImageMetadataHandler

---
+++ src/main/java/jatoo/image/ImageMetadataHandler.java
@@ -0,0 +1,83 @@
+package jatoo.image;
+
+import java.io.File;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
+/**
+ * A collection of utility methods to ease the work with image metadata.
+ * 
+ * @author <a href="http://cristian.sulea.net" rel="author">Cristian Sulea</a>
+ * @version 1.0-SNAPSHOT, May 26, 2017
+ */
+public abstract class ImageMetadataHandler {
+
+  /** The logger. */
+  private static final Log logger = LogFactory.getLog(ImageMetadataHandler.class);
+
+  public static final String HANDLER_EXIFTOOL = "ExifTool";
+
+  private static ImageMetadataHandler defaultInstance;
+  private static Map<String, ImageMetadataHandler> availableInstances = new HashMap<>();
+
+  static {
+
+    Map<String, String> handlerClasses = new LinkedHashMap<>();
+    handlerClasses.put(HANDLER_EXIFTOOL, "jatoo.image.metadata.exiftool.ExifToolImageMetadataHandler");
+
+    for (String handler : handlerClasses.keySet()) {
+
+      try {
+
+        availableInstances.put(handler, (ImageMetadataHandler) Class.forName(handlerClasses.get(handler)).newInstance());
+
+        if (defaultInstance == null) {
+          defaultInstance = availableInstances.get(handler);
+        }
+      }
+
+      catch (InstantiationException | IllegalAccessException | ClassNotFoundException e) {
+        e.printStackTrace();
+        logger.info("", e);
+      }
+    }
+  }
+
+  public static ImageMetadataHandler getInstance() {
+    return defaultInstance;
+  }
+
+  public static ImageMetadataHandler getInstance(String handler) {
+    return availableInstances.get(handler);
+  }
+
+  public static void setDefaultInstance(ImageMetadataHandler defaultInstance) {
+    ImageMetadataHandler.defaultInstance = defaultInstance;
+  }
+
+  protected ImageMetadataHandler() {}
+
+  public ImageMetadata getMetadata(String image) {
+    return getMetadata(new File(image));
+  }
+
+  public abstract ImageMetadata getMetadata(File image);
+
+  public Date getDateTimeOriginal(String image) {
+    return getDateTimeOriginal(new File(image));
+  }
+
+  public abstract Date getDateTimeOriginal(File image);
+
+  public boolean copyMetadata(String srcImage, String dstImage) {
+    return copyMetadata(new File(srcImage), new File(dstImage));
+  }
+
+  public abstract boolean copyMetadata(File srcImage, File dstImage);
+
+}
